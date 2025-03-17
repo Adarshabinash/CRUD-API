154a7f5675c3ff4055b5fd98e1d600f8d5da0f36
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2025-03-17 12:50:08 +0530

    changes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
-
 const connection = require("./connection");
 
 app.use(express.json()); //To handle the response since they will be in json.
